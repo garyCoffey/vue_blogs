38f3175cafbd55c90e20d5d2849f038ffd6f7ea8
Gary Coffey <garycoffey12@gmail.com>
2021-09-18 13:33:18 -0500

    adds_game

--- pages/games/index.vue
+++ pages/games/index.vue
@@ -1,6 +1,59 @@
 <template>
-  <h1>Games</h1>
+  <div>
+    <h1>Games</h1>
+    <v-card
+      class="mx-auto"
+      max-width="344"
+    >
+      <v-img
+        src="https://media.giphy.com/media/NXOVucluUUdeE/giphy.gif"
+        height="200px"
+      ></v-img>
+
+      <v-card-title>
+        Monster Slayer
+      </v-card-title>
+
+      <v-card-actions>
+        <nav>
+          <NuxtLink to="/games/monsterSlayer">
+            <v-btn
+              color="orange lighten-2"
+              text
+            >
+              Play
+            </v-btn>
+          </NuxtLink>
+        </nav>
+
+        <v-spacer></v-spacer>
+
+        <v-btn
+          icon
+          @click="show = !show"
+        >
+          <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+        </v-btn>
+      </v-card-actions>
+
+      <v-expand-transition>
+        <div v-show="show">
+          <v-divider></v-divider>
+
+          <v-card-text>
+            A turn based game where you will fight to defeat the monster, before you yourself are defeated.
+          </v-card-text>
+        </div>
+      </v-expand-transition>
+    </v-card>
+  </div>
 </template>
 <script>
-  // import app from 'monster-slayer'
+  export default {
+    data() {
+      return {
+        show: false,
+      }
+    }
+  }
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,15 @@
 <template>
-  <h1>Home</h1>
+  <div>
+    <h1>Home</h1>
+    <v-card
+      elevation="2"
+    >
+      <v-card-title>
+        Welcome to Play & Code
+      </v-card-title>
+      <v-card-subtitle>
+        I hope you enjoy reading the blog posts and playing the available games!
+      </v-card-subtitle>
+    </v-card>
+  </div>
 </template>
